fa0f24c066e16255aab12a21aa7fae37fab7a6ff
Phil Norton <phil@craftydevil.co.uk>
2017-11-08 15:18:21 +0000

    Using update, rather than save, to avoid validation errors when trying to save the User model

--- lib/devise_lastseenable/model.rb
+++ lib/devise_lastseenable/model.rb
@@ -4,10 +4,7 @@ module Devise
   module Models
     module Lastseenable
       def stamp!
-        if self.last_seen.to_i < (Time.now - 5.minutes).to_i
-          self.last_seen = DateTime.now
-          self.save!
-        end
+        self.update_column(:last_seen, DateTime.now) if self.last_seen.to_i < (Time.now - 5.minutes).to_i
       end
     end
   end
